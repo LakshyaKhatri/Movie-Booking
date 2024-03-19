98c1889039b4dc27ff350186b73f2fe197184883
Lakshya Khatri <lakshyak67@gmail.com>
2024-03-19 22:05:53 +0530

    feat: allow accessing genres and shows

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,9 +1,28 @@
 class Movie
   attr_reader :title
 
+  # Skipping the following tasks to prevent scope creep:
+  # - multiple shows on same timestamp
   def initialize(title:)
     @title = title.squeeze.strip.capitalize
     @genres = []
     @shows = {}
+  end
+
+  def genres
+    @genres.dup
+  end
+
+  def add_genre(genre)
+    @genres << genre
+  end
+
+  def add_show(show_timing, rows, cols)
+    show_timing = show_timing.upcase
+    # TODO: create and add a show
+  end
+
+  def show_timings
+    @shows.keys
   end
 end
