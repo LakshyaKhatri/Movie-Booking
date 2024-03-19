d0df867394867a7de7bca88c3cffe02893ee5f0c
Lakshya Khatri <lakshyak67@gmail.com>
2024-03-19 22:21:21 +0530

    feat: validate show timings

--- lib/movie.rb
+++ lib/movie.rb
@@ -1,3 +1,5 @@
+require_relative 'utils'
+
 class Movie
   attr_reader :title
 
@@ -20,7 +22,10 @@ class Movie
 
   def add_show(show_timing, rows, cols)
     show_timing = show_timing.upcase
+    validate_show_timings(show_timing)
+
     # TODO: create and add a show
+    @shows[show_timing] = Object.new
   end
 
   def show_timings
@@ -31,6 +36,17 @@ class Movie
 
   def validate_genre(genre)
     return if Utils::ALL_GENRES.include?(genre)
+
     raise ArgumentError, "Invalid genre, please see data/genres.json for a complete list of genres"
+  end
+
+  def validate_show_timings(show_timing)
+    begin
+      DateTime.strptime(show_timing, "%I:%M %p")
+    rescue ArgumentError
+      raise ArgumentError, "Invalid show timings #{show_timing}, please use HH:MM AM/PM format"
+    end
+
+    raise ArgumentError, "A show already exists at #{show_timing} for this movie" if @shows.key?(show_timing)
   end
 end
