bc3e93036090b3ef70a14bb22a13b22563e7b0b1
Lakshya Khatri <lakshyak67@gmail.com>
2024-03-21 09:45:23 +0530

    chore: introduce main gem file

---
+++ lib/movie_booking.rb
@@ -0,0 +1,8 @@
+require 'tty-prompt'
+require 'json'
+
+require_relative 'movie_booking/booking'
+require_relative 'movie_booking/movie'
+require_relative 'movie_booking/show'
+require_relative 'movie_booking/ticket'
+require_relative 'movie_booking/utils'
